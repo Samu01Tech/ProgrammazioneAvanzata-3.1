bd4898ac8669fa8b4a8993ec91ef136fe6e1e9f6
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-09-29 13:49:56 +0000

    Costruttori e Distruttori A

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class A{
+    int k;
+    float h;
+    public: 
+    //Costruttori (0 e più parametri)
+    A(){
+        k = 0;
+        h = 0.0;
+    }
+    A(int _k, float _h){
+        k = _k;
+        h = _h;
+    }
+    //Costruttore di Copia
+    A(const A &cls){
+        k = cls.k;
+        h = cls.h;
+        cout << "Costruttore di copia" << endl;
+    }
+    //Distruttore
+    ~A(){
+        cout << "Distruttore A" << endl;
+    };
+};
+
 int main() {
   cout << "Hello World!\n";
 }
